e309271e345bac8ccb29718ff186395e00ac750c
bsorrentino <bartolomeo.sorrentino@gmail.com>
2023-08-16 16:04:33 +0200

    Initial Commit

---
+++ AI-MultiPrompt/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  AI-MultiPrompt
+//
+//  Created by Bartolomeo Sorrentino on 16/08/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
