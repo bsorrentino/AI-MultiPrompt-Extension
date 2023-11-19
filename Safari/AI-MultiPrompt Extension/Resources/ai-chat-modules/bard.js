37bfeab21cce894db2b1c60900e9fb85ee637327
bsorrentino <bartolomeo.sorrentino@gmail.com>
2023-11-19 13:25:38 +0100

    feat: add support of copilot chat
        add usage of esm module

---
+++ Safari/AI-MultiPrompt Extension/Resources/ai-chat-modules/bard.js
@@ -0,0 +1,38 @@
+
+export const queryTab = () => chrome.tabs.query({ url: "*://bard.google.com/*" , currentWindow:true})
+
+/**
+ * Submits the given prompt text to the Bard playground form.
+ * 
+ * take note that this function must be serializable to pass to executeScript. Don't call subfunction etc.
+ * 
+ * @param {string} prompt - The prompt text to submit.
+ */
+export const submit = (prompt) => {
+
+    const promptElem = document.querySelector("input-area rich-textarea > div");
+    if (!promptElem) {
+        console.warn("prompt not found!")
+        return;
+    }
+    
+    promptElem.innerHTML = prompt
+
+    const buttons = document.querySelectorAll("input-area button");
+    
+    if (buttons && buttons.length > 0) {
+
+        buttons.forEach(b => console.debug("BUTTON => ", b));
+
+        const submitButton = buttons[buttons.length - 1];
+
+        // submitButton.click() // just seems doesn't work with one click
+        setTimeout( () => {
+            submitButton.click()
+            // alternative implementation
+            // submitButton.dispatchEvent(new Event('click', { 'bubbles': true })); 
+        }, 800);
+        
+    }
+
+}
