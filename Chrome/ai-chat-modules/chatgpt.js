c88513387e319ad9da9558a49a3f6bb7bdda6bbc
bsorrentino <bartolomeo.sorrentino@gmail.com>
2023-11-19 13:21:41 +0100

    feat: add support of copilot chat
    add usage of esm module

---
+++ Chrome/ai-chat-modules/chatgpt.js
@@ -0,0 +1,39 @@
+
+export const queryTab = () => chrome.tabs.query({ url: "*://*.openai.com/*", currentWindow:true })
+
+/**
+ * Submits the given prompt text to the OpenAI playground form.
+ * 
+ * take note that this function must be serializable to pass to executeScript. Don't call subfunction etc.
+ * 
+ * @param {string} prompt - The prompt text to submit.
+ */
+export const submit = (prompt) => {
+
+    const promptElem = document.querySelector("form #prompt-textarea");
+
+    if (!promptElem) {
+        console.warn("prompt not found!");
+        return;
+    }
+
+    promptElem.value = prompt;
+
+    promptElem.dispatchEvent(new Event('input', { 'bubbles': true }));
+
+    const parentForm = promptElem.closest("form");
+
+    console.debug("FORM => ", parentForm);
+
+    const buttons = parentForm.querySelectorAll("button");
+
+    if (buttons && buttons.length > 0) {
+
+        buttons.forEach(b => console.debug("BUTTON => ", b));
+
+        const submitButton = buttons[buttons.length - 1];
+        submitButton.click();
+
+    }
+
+}
