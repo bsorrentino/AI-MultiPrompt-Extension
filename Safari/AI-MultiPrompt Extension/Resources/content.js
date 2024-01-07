5c066783f09e7fb45ae425216f730330be0318f4
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-01-07 15:15:03 +0100

    feat: integrate deepseek
    working on #8

--- Safari/AI-MultiPrompt Extension/Resources/content.js
+++ Safari/AI-MultiPrompt Extension/Resources/content.js
@@ -1,6 +1,22 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+// browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
+//     console.log("Received response: ", response);
+// });
+
+const setTheme = ( mql ) => {
+    if( mql.matches ) {
+        console.log( "dark mode" )
+        browser.runtime.sendMessage({ theme: 'dark' });
+    } else {
+        browser.runtime.sendMessage({ theme: 'light' });
+        console.log( "light mode" )
+    }
+}
+
+const mediaQueryForDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
+
+setTheme( mediaQueryForDarkScheme )
+
+mediaQueryForDarkScheme.addEventListener( 'change', setTheme ) 
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
